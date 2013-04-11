47de0b1bea062df76bd90132ff301d0a0962f4c7
mahbam42 <max.brother@gmail.com>
2013-04-10 22:37:46 -0700

    added comments for clarity

--- scripts/dragAndDrop.js
+++ scripts/dragAndDrop.js
@@ -1,23 +1,24 @@
 
 $(document).ready(function () {
+    // Stretch shifts to the desired length
     function stretch(e) {
         $(e).resizable({
             maxHeight: 50,
-            maxWidth: 1200,
-            minHeight: 25,
-            minWidth: 44,
-            grid: 25,
-            ghost: true
+            maxWidth: 1200, // 24 (hours) x 50px
+            minHeight: 25, 
+            minWidth: 44, // 4 hour minimum for shift length (hard coded atm)
+            grid: 25, // Snap to the 1/2 hour 
+            ghost: true // because it looks cool 
         });
-    }
+    }// Drag shifts around to change start time 
     function drag(e) {
         $(e).draggable({
-            snap: true,
-            grid: [25, 50],
-            containment: "parent",
-            axis: "x"
+            snap: true, 
+            grid: [25, 50], //Snaps to the half hour 
+            containment: "parent", // keeps each shift in their row
+            axis: "x" // pretty clear 
         });
-    }
+    }// Deletes a shift 
     function closer(e) {
         $(e).find('.close').click(function () {
             $(this).parent().remove();
@@ -26,6 +27,7 @@ $(document).ready(function () {
     // Set Up an Empty Shift 
     shift = '<div class="shift"><span class="close">X</span><p>Regular Shift</p></div>';
 
+    // Add a new shift when you click on name 
     $('.name').click(function (event) {
         var $newshift = $(shift);
             $(this).siblings(".row").append($newshift);
@@ -33,4 +35,6 @@ $(document).ready(function () {
             drag($newshift);
             closer($newshift);
     });
+
+
 });
