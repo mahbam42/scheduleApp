d6ac0d03c89fd556be650d48244011a6d80ab06c
mahbam42 <max.brother@gmail.com>
2013-04-11 00:27:47 -0700

    fixed the scope on some functions

--- scripts/dragAndDrop.js
+++ scripts/dragAndDrop.js
@@ -1,29 +1,32 @@
+// Stretch shifts to the desired length
+function stretch(e) {
+    $(e).resizable({
+        maxHeight: 50,
+        maxWidth: 1200, // 24 (hours) x 50px
+        minHeight: 25, 
+        minWidth: 44, // 4 hour minimum for shift length (hard coded atm)
+        grid: 25, // Snap to the 1/2 hour 
+        ghost: true // because it looks cool 
+    });
+}
+// Drag shifts around to change start time 
+function drag(e) {
+    $(e).draggable({
+        snap: true, 
+        grid: [25, 50], //Snaps to the half hour 
+        containment: "parent", // keeps each shift in their row
+        axis: "x" // pretty clear 
+    });
+}
+// Deletes a shift 
+function closer(e) {
+    $(e).find('.close').click(function () {
+        $(this).parent().remove();
+    });
+}
+
 
 $(document).ready(function () {
-    // Stretch shifts to the desired length
-    function stretch(e) {
-        $(e).resizable({
-            maxHeight: 50,
-            maxWidth: 1200, // 24 (hours) x 50px
-            minHeight: 25, 
-            minWidth: 44, // 4 hour minimum for shift length (hard coded atm)
-            grid: 25, // Snap to the 1/2 hour 
-            ghost: true // because it looks cool 
-        });
-    }// Drag shifts around to change start time 
-    function drag(e) {
-        $(e).draggable({
-            snap: true, 
-            grid: [25, 50], //Snaps to the half hour 
-            containment: "parent", // keeps each shift in their row
-            axis: "x" // pretty clear 
-        });
-    }// Deletes a shift 
-    function closer(e) {
-        $(e).find('.close').click(function () {
-            $(this).parent().remove();
-        });
-    }
     // Set Up an Empty Shift 
     shift = '<div class="shift"><span class="close">X</span><p>Regular Shift</p></div>';
 
